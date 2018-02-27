54fbbbff42984ffff059fb3945a0d693ba2db97b
Daniel Hicks <misterhicks81@gmail.com>
2018-02-26 23:50:51 -0800

    updated posts_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,10 @@
 class PostsController < ApplicationController
+  before_action :authorize_post_view, only: :show 
+  
   def create
+      @post = current_user.posts.new post_params
+      @post.save
+      redirect_to post_path(@post.id)
   end
 
   def destroy
@@ -12,11 +17,28 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = current_user.posts.new
+    
   end
 
   def show
+
+    
   end
 
   def update
   end
 end
+
+  def authorize_post_view 
+  @post = Post.find params[:id]
+  if  @post.user_id != current_user.id
+  redirect_to root_path
+  end
+end
+
+private
+    def post_params
+      return params.require(:post).permit(:title, :body)
+    end
+  end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,11 @@ class UsersController < ApplicationController
   before_action :authorize, only: [:show]
 
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def new
